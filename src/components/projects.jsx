fcd4614f91cec53a18c24f7d3df7bab04fed43a7
JoshMurunga <joshmurush@gmail.com>
2020-08-08 15:42:05 +0300

    projects wip

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 
 class Projects extends Component {
-  state = {};
   render() {
-    return <h1 style={{ marginTop: "50px" }}>Projects</h1>;
+    return (
+      <div className="contact-component contact-scroll">
+        <div className="contact-me">PROJECTS</div>
+        <div className="text-center">
+          I am always looking to add to my portfolio. Here are some of the
+          projects that I have worked on over the years.
+        </div>
+        <div className="projects-container">
+          <div className="project-box" id="p-box">
+            <div className="project-box-image"></div>
+            <hr />
+            <div className="project-box-info"></div>
+            <div className="project-box-links">this is links area</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
