6eeb5ca455b4929f93895bc4b11165f6fdd7f87f
JoshMurunga <joshmurush@gmail.com>
2020-08-10 20:10:18 +0300

    project box complete

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 class Home extends Component {
   state = {};
   render() {
-    return <h1 style={{ marginTop: "50px" }}>home</h1>;
+    return (
+      <div className="container">
+        <h1 style={{ marginTop: "50px" }}>home</h1>
+        <div className="tipper">
+          Hover over me
+          <span className="tippertext">Tooltip text</span>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import reactIcon from "@iconify/icons-logos/react";
+import herokuIcon from "@iconify/icons-logos/heroku-icon";
+import gitIcon from "@iconify/icons-logos/git-icon";
 
 class Projects extends Component {
   render() {
@@ -11,10 +15,62 @@ class Projects extends Component {
         </div>
         <div className="projects-container">
           <div className="project-box" id="p-box">
-            <div className="project-box-image"></div>
+            <div className="project-box-image">
+              <img
+                src="portfolio.jpg"
+                alt="Portfolio Website"
+                className="project-img"
+              />
+            </div>
             <hr />
-            <div className="project-box-info"></div>
-            <div className="project-box-links">this is links area</div>
+            <div className="project-box-info">
+              <a
+                href="https://joshmurunga.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cv-email pb-info"
+              >
+                My Porfolio Website
+              </a>
+              <div className="project-box-details">
+                A fully responsive portfolio website for myself to showcase my
+                work.
+              </div>
+            </div>
+            <div className="project-box-links">
+              <div className="pb-links-fl">
+                <div className="tipper">
+                  <Icon icon={reactIcon} className="mr-1" />
+                  <span className="tippertext">React</span>
+                </div>
+                <div className="tipper">
+                  <Icon icon={herokuIcon} className="mr-1" />
+                  <span className="tippertext">Heroku</span>
+                </div>
+                <div className="tipper">
+                  <Icon icon={gitIcon} />
+                  <span className="tippertext">Git</span>
+                </div>
+              </div>
+              <div className="pb-links-fr">
+                <a
+                  href="https://github.com/JoshMurunga/portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-warning mr-2"
+                >
+                  Code
+                </a>
+                <a
+                  href="https://joshmurunga.herokuapp.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn btn-danger"
+                >
+                  visit
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
